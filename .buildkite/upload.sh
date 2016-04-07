102a3cb9bbbae2d0825fafc9452527f112712b2f
Lachlan Donald <lachlan@ljd.cc>
2016-04-07 13:25:28 +1000

    Create missing build/ dir in deploy

--- .buildkite/upload.sh
+++ .buildkite/upload.sh
@@ -5,5 +5,7 @@ export AWS_REGION=us-east-1
 export AWS_ACCESS_KEY_ID=$SANDBOX_AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$SANDBOX_AWS_SECRET_ACCESS_KEY
 
+mkdir -p build/
 buildkite-agent artifact download "build/*" build/
+
 make upload branch=${BUILDKITE_BRANCH}
