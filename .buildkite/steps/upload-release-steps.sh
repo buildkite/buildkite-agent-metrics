ca9fdca99ee6802fef4ba5aae92368f97f138584
Lachlan Donald <lachlan@buildkite.com>
2017-11-23 16:45:25 +1100

    Use correct VERSION

--- .buildkite/steps/upload-release-steps.sh
+++ .buildkite/steps/upload-release-steps.sh
@@ -5,12 +5,12 @@ export VERSION=$(awk -F\" '/const Version/ {print $2}' version/version.go)
 
 git fetch --tags
 
-echo "Checking if $version is a tag..."
+echo "Checking if $VERSION is a tag..."
 
 # If there is already a release (which means a tag), we want to avoid trying to create
 # another one, as this will fail and cause partial broken releases
-if git rev-parse -q --verify "refs/tags/v${version}" ; then
-  echo "Tag refs/tags/v${version} already exists"
+if git rev-parse -q --verify "refs/tags/v${VERSION}" ; then
+  echo "Tag refs/tags/v${VERSION} already exists"
   exit 0
 fi
 
