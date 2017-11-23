bba4e8e3f2ecdc2061ed0239c2324584484fb98e
Lachlan Donald <lachlan@buildkite.com>
2017-11-23 16:03:57 +1100

    Use correct dist path

--- .buildkite/steps/build-lambda.sh
+++ .buildkite/steps/build-lambda.sh
@@ -4,9 +4,10 @@ set -eu
 go_pkg="github.com/buildkite/buildkite-metrics"
 go_src_dir="/go/src/${go_pkg}"
 version=$(awk -F\" '/const Version/ {print $2}' version/version.go)-${BUILDKITE_BUILD_NUMBER}
+dist_file="build/buildkite-metrics-${version}-lambda.zip"
 
 docker run --rm -v "${PWD}:${go_src_dir}" -w "${go_src_dir}" eawsy/aws-lambda-go
 mkdir -p build/
-mv handler.zip "build/buildkite-metrics-${version}-lambda.zip"
+mv handler.zip "$dist_file"
 
-buildkite-agent artifact upload "${DISTFILE}"
+buildkite-agent artifact upload "$dist_file"
