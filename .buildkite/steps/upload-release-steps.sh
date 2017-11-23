b02d6394e6c165f3973f859b40fd8e43cb4fe387
Lachlan Donald <lachlan@buildkite.com>
2017-11-23 16:22:27 +1100

    Dynamically generate release pipeline

---
+++ .buildkite/steps/upload-release-steps.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -euo pipefail
+
+block_step() {
+  local label="$1"
+  cat <<YAML
+  - wait
+  - block: "${label}"
+YAML
+}
+
+release_step() {
+  local label="$1"
+  local command="$2"
+  cat <<YAML
+  - block
+  - label: "${label}"
+    command: "${command}"
+    branches: master
+    agents:
+      queue: "deploy"
+    concurrency: 1
+    concurrency_group: 'release_buildkite_metrics'
+YAML
+}
+
+output_steps_yaml() {
+  local version="$1"
+
+  echo "steps:"
+
+  block_step "release?"
+  command_step ":s3: $version" ".buildkite/scripts/upload-to-s3.sh"
+}
+
+version=$(awk -F\" '/const Version/ {print $2}' version/version.go)
+
+git fetch --tags
+
+echo "Checking if $version is a tag..."
+
+# If there is already a release (which means a tag), we want to avoid trying to create
+# another one, as this will fail and cause partial broken releases
+if git rev-parse -q --verify "refs/tags/v${version}" >/dev/null; then
+  echo "Tag refs/tags/v${agent_version} already exists"
+  exit 0
+fi
+
+output_steps_yaml "$version" | buildkite-agent pipeline upload
---
+++ .buildkite/steps/upload-to-s3.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -eu
+
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-$SANDBOX_AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-$SANDBOX_AWS_SECRET_ACCESS_KEY}
+
+EXTRA_REGIONS=(
+	us-east-2
+	us-west-1
+	us-west-2
+	eu-west-1
+	eu-west-2
+	eu-central-1
+	ap-northeast-1
+	ap-northeast-2
+	ap-southeast-1
+	ap-southeast-2
+	ap-south-1
+	sa-east-1
+)
+
+BASE_BUCKET=buildkite-metrics
+
+if [[ -n "${BUILDKITE:-}" ]] ; then
+	echo "~~~ :buildkite: Downloading artifacts"
+	mkdir -p build/
+	buildkite-agent artifact download "build/*" build/
+fi
+
+echo "+++ :s3: Uploading files to ${BASE_BUCKET} in ${AWS_DEFAULT_REGION}"
+aws s3 sync --acl public-read ./build "s3://${BASE_BUCKET}/"
+for f in build/* ;
+	do echo "https://s3.amazonaws.com/bucket/$f"
+done
+
+for region in "${EXTRA_REGIONS[@]}" ; do
+	bucket="${BASE_BUCKET}-${region}"
+	echo "+++ :s3: Copying files to ${bucket}"
+	if ! aws s3api head-bucket --bucket "${bucket}" --region "${region}" &> /dev/null ; then
+		echo "Creating s3://${bucket}/"
+		aws s3 mb "s3://${bucket}/" --region "${region}"
+	fi
+	aws --region "${region}" s3 sync --exclude "*" --include "*.zip" --delete --acl public-read "s3://${BASE_BUCKET}/" "s3://${bucket}/"
+	for f in build/* ; do
+		echo "https://${bucket}.s3-${region}.amazonaws.com/$f"
+	done
+done
+
+echo "+++ :s3: Uploading binary to s3://${BASE_BUCKET}"
+aws s3 cp --acl public-read build/buildkite-metrics-Linux-x86_64* "s3://${BASE_BUCKET}/buildkite-metrics-Linux-x86_64"
