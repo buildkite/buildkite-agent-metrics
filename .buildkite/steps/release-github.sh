4832773c24ecf5de7a6e0c61998e0b0394ceb142
Lachlan Donald <lachlan@buildkite.com>
2017-11-27 13:10:02 +1100

    Pass in artifacts to github-release

--- .buildkite/steps/release-github.sh
+++ .buildkite/steps/release-github.sh
@@ -14,7 +14,7 @@ rm -rf dist
 mkdir -p dist
 buildkite-agent artifact download "dist/*" .
 
-docker run -v "${PWD}/dist:/dist" -e GITHUB_RELEASE_ACCESS_TOKEN --rm "buildkite/github-release" "v${version}" /dist/* \
+docker run -v "$PWD:$PWD" -e GITHUB_RELEASE_ACCESS_TOKEN --rm "buildkite/github-release" "v${version}" dist/* \
   --commit "${BUILDKITE_COMMIT}" \
   --tag "v${version}" \
   --github-repository "buildkite/buildkite-metrics"
