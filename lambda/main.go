5d0939009048fda3a278218188bcd61f824a3704
Lachlan Donald <lachlan@buildkite.com>
2018-11-01 09:32:36 +1100

    Move to github.com/aws/aws-lambda-go/lambda

--- lambda/main.go
+++ lambda/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -9,13 +10,24 @@ import (
 	"strings"
 	"time"
 
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/buildkite/buildkite-agent-metrics/backend"
 	"github.com/buildkite/buildkite-agent-metrics/collector"
 	"github.com/buildkite/buildkite-agent-metrics/version"
-	"github.com/eawsy/aws-lambda-go/service/lambda/runtime"
 )
 
-func handle(evt json.RawMessage, ctx *runtime.Context) (interface{}, error) {
+func main() {
+	if os.Getenv(`DEBUG`) != "" {
+		_, err := Handler(context.Background(), json.RawMessage([]byte{}))
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		lambda.Start(Handler)
+	}
+}
+
+func Handler(ctx context.Context, evt json.RawMessage) (string, error) {
 	token := os.Getenv("BUILDKITE_AGENT_TOKEN")
 	backendOpt := os.Getenv("BUILDKITE_BACKEND")
 	queue := os.Getenv("BUILDKITE_QUEUE")
@@ -72,8 +84,4 @@ func handle(evt json.RawMessage, ctx *runtime.Context) (interface{}, error) {
 
 	log.Printf("Finished in %s", time.Now().Sub(t))
 	return "", nil
-}
-
-func init() {
-	runtime.HandleFunc(handle)
 }
