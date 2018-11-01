ccfabaf0b8a5dc5afc3858934af1da88d7327602
Lachlan Donald <lachlan@buildkite.com>
2018-11-01 13:39:08 +1100

    Upload lambda for every build

--- .buildkite/steps/build-lambda.sh
+++ .buildkite/steps/build-lambda.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -eu
 
-go_pkg="github.com/buildkite/buildkite-agent-metrics"
-go_src_dir="/go/src/${go_pkg}"
-version=$(awk -F\" '/const Version/ {print $2}' version/version.go)
-dist_file="dist/buildkite-agent-metrics-v${version}-lambda.zip"
-
-docker run --rm --volume "$PWD:${go_src_dir}" \
-  --workdir "${go_src_dir}" \
-  --env GO111MODULE=on \
+docker run --rm --volume "$PWD:/code" \
+  --workdir "/code" \
   --rm \
     golang:1.11 \
     sh -c "go get ./lambda && go build -o ./lambda/handler ./lambda"
@@ -16,6 +10,6 @@ docker run --rm --volume "$PWD:${go_src_dir}" \
 chmod +x ./lambda/handler
 
 mkdir -p dist/
-zip -j "$dist_file" lambda/handler
+zip -j handler.zip lambda/handler
 
-buildkite-agent artifact upload "$dist_file"
+buildkite-agent artifact upload handler.zip
--- .buildkite/steps/upload-to-s3.sh
+++ .buildkite/steps/upload-to-s3.sh
@@ -2,8 +2,6 @@
 set -eu
 
 export AWS_DEFAULT_REGION=us-east-1
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-$SANDBOX_AWS_ACCESS_KEY_ID}
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-$SANDBOX_AWS_SECRET_ACCESS_KEY}
 
 EXTRA_REGIONS=(
 	us-east-2
@@ -20,30 +18,24 @@ EXTRA_REGIONS=(
 	sa-east-1
 )
 
-BASE_BUCKET=buildkite-metrics
+VERSION=$(awk -F\" '/const Version/ {print $2}' version/version.go)
+BASE_BUCKET=buildkite-lambdas
+BUCKET_PATH="buildkite-agent-metrics"
+
+if [[ "${1:-}" == "release" ]] ; then
+  BUCKET_PATH="${BUCKET_PATH}/v${VERSION}"
+else
+  BUCKET_PATH="${BUCKET_PATH}/builds/${BUILDKITE_BUILD_NUMBER}"
+fi
 
 echo "~~~ :buildkite: Downloading artifacts"
-mkdir -p dist/
-buildkite-agent artifact download "dist/*" dist/
-buildkite-agent artifact download "buildkite-agent-metrics-*" dist/
+buildkite-agent artifact download handler.zip .
 
-ls -al dist/
-
-echo "+++ :s3: Uploading lambda to ${BASE_BUCKET} in ${AWS_DEFAULT_REGION}"
-aws s3 sync --acl public-read ./dist "s3://${BASE_BUCKET}/"
-for f in build/* ;
-	do echo "https://s3.amazonaws.com/bucket/$f"
-done
+echo "--- :s3: Uploading lambda to ${BASE_BUCKET}/${BUCKET_PATH}/ in ${AWS_DEFAULT_REGION}"
+aws s3 cp --acl public-read handler.zip "s3://${BASE_BUCKET}/${BUCKET_PATH}/handler.zip"
 
 for region in "${EXTRA_REGIONS[@]}" ; do
 	bucket="${BASE_BUCKET}-${region}"
-	echo "+++ :s3: Copying files to ${bucket}"
-	if ! aws s3api head-bucket --bucket "${bucket}" --region "${region}" &> /dev/null ; then
-		echo "Creating s3://${bucket}/"
-		aws s3 mb "s3://${bucket}/" --region "${region}"
-	fi
-	aws --region "${region}" s3 sync --exclude "*" --include "*.zip" --delete --acl public-read "s3://${BASE_BUCKET}/" "s3://${bucket}/"
-	for f in build/* ; do
-		echo "https://${bucket}.s3-${region}.amazonaws.com/$f"
-	done
+	echo "--- :s3: Copying files to ${bucket}"
+	aws --region "${region}" s3 cp --acl public-read "s3://${BASE_BUCKET}/${BUCKET_PATH}/handler.zip" "s3://${bucket}/${BUCKET_PATH}/handler.zip"
 done
