a24b8d5ae273c3454cc3ed214aa0329bbd6c5291
Lachlan Donald <lachlan@buildkite.com>
2019-03-01 07:39:17 +1100

    Update bucket publishing for new regions

--- .buildkite/steps/upload-to-s3.sh
+++ .buildkite/steps/upload-to-s3.sh
@@ -3,19 +3,25 @@ set -eu
 
 export AWS_DEFAULT_REGION=us-east-1
 
+# Updated from https://docs.aws.amazon.com/general/latest/gr/rande.html#lambda_region
 EXTRA_REGIONS=(
-	us-east-2
-	us-west-1
-	us-west-2
-	eu-west-1
-	eu-west-2
-	eu-central-1
-	ap-northeast-1
-	ap-northeast-2
-	ap-southeast-1
-	ap-southeast-2
-	ap-south-1
-	sa-east-1
+  us-east-2
+  us-west-1
+  us-west-2
+  ap-south-1
+  ap-northeast-2
+  ap-southeast-1
+  ap-southeast-2
+  ap-northeast-1
+  ca-central-1
+  cn-north-1
+  cn-northwest-1
+  eu-central-1
+  eu-west-1
+  eu-west-2
+  eu-west-3
+  eu-north-1
+  sa-east-1
 )
 
 VERSION=$(awk -F\" '/const Version/ {print $2}' version/version.go)
