0e08a5d5c02335bb23ce5b7b15cd5558cda98dd0
Mark Wolfe <mark.wolfe@buildkite.com>
2024-06-05 15:36:29 +1000

    Updated test to github.com/cweill/gotests/gotests to simplify

--- lambda/main_test.go
+++ lambda/main_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func Test_toIntWithDefault(t *testing.T) {
 	type args struct {
-		value    string
-		defaults int
+		val        string
+		defaultVal int
 	}
 	tests := []struct {
 		name    string
@@ -15,28 +15,29 @@ func Test_toIntWithDefault(t *testing.T) {
 	}{
 		{
 			name: "empty",
-			args: args{value: "", defaults: 10},
+			args: args{val: "", defaultVal: 10},
 			want: 10,
 		},
 		{
 			name:    "invalid",
-			args:    args{value: "invalid", defaults: 10},
+			args:    args{val: "invalid", defaultVal: 10},
 			wantErr: true,
 		},
 		{
 			name: "valid",
-			args: args{value: "20", defaults: 10},
+			args: args{val: "20", defaultVal: 10},
 			want: 20,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got, err := toIntWithDefault(tt.args.value, tt.args.defaults); (err != nil) != tt.wantErr {
-				t.Errorf("toIntWithDefault(%q, %d) error = %v, wantErr %v", tt.args.value, tt.args.defaults, err, tt.wantErr)
-			} else {
-				if got != tt.want {
-					t.Errorf("toIntWithDefault(%q, %d) = %v, want %v", tt.args.value, tt.args.defaults, got, tt.want)
-				}
+			got, err := toIntWithDefault(tt.args.val, tt.args.defaultVal)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("toIntWithDefault(%q, %d) error = %v, wantErr %v", tt.args.val, tt.args.defaultVal, err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("toIntWithDefault(%q, %d) = %v, want %v", tt.args.val, tt.args.defaultVal, got, tt.want)
 			}
 		})
 	}
