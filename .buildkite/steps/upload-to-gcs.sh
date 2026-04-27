6a7acf79bf10b614cfcf2138ce579a8f61ffc37f
Owen Mehegan <owen@buildkite.com>
2026-04-27 13:52:22 +1000

    Temporary: let the release pipeline run on a branch

--- .buildkite/steps/upload-to-gcs.sh
+++ .buildkite/steps/upload-to-gcs.sh
@@ -5,27 +5,12 @@ export GCP_DEFAULT_REGION="us-central1"
 
 VERSION="$(awk -F\" '/const Version/ {print $2}' version/version.go)"
 BASE_BUCKET="buildkite-cloud-functions"
-BUCKET_PATH="buildkite-agent-metrics"
+BUCKET_PATH="buildkite-agent-metrics/builds/${BUILDKITE_BUILD_NUMBER}"
 ARTIFACT="buildkite-agent-metrics.zip"
 
-if [[ "${1:-}" == "release" ]] ; then
-  BUCKET_PATH="${BUCKET_PATH}/v${VERSION}"
-else
-  BUCKET_PATH="${BUCKET_PATH}/builds/${BUILDKITE_BUILD_NUMBER}"
-fi
-
-echo "~~~ :buildkite: Downloading artifacts"
-
+echo "~~~ :hammer: Creating dummy artifact for testing"
 mkdir -p dist
-if [[ "${1:-}" == "release" ]] ; then
-  buildkite-agent artifact download --build "${BUILDKITE_TRIGGERED_FROM_BUILD_ID}" dist/"${ARTIFACT}" ./dist
-else
-  buildkite-agent artifact download dist/"${ARTIFACT}" ./dist
-fi
+echo "test" > dist/"${ARTIFACT}"
 
 echo "--- :GCS: Uploading cloud function to ${BASE_BUCKET}/${BUCKET_PATH}/ in ${GCP_DEFAULT_REGION}"
 gcloud storage cp dist/"${ARTIFACT}" "gs://${BASE_BUCKET}/${BUCKET_PATH}/${ARTIFACT}"
-
-if [[ "${1:-}" == "release" ]] ; then
-  gcloud storage cp "gs://${BASE_BUCKET}/${BUCKET_PATH}/${ARTIFACT}" "gs://${BASE_BUCKET}/buildkite-agent-metrics/cloud-function-latest.zip"
-fi
