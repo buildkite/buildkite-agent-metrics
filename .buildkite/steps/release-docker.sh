6d7d0934e2a453b320a1557aed2fc6a1f6f61369
Owen Mehegan <owen@buildkite.com>
2026-04-27 14:13:59 +1000

    Revert test hacks, keep propagate-gcp-auth-tokens fix

--- .buildkite/steps/release-docker.sh
+++ .buildkite/steps/release-docker.sh
@@ -4,7 +4,7 @@ set -Eeufo pipefail
 
 source .buildkite/lib/release_dry_run.sh
 
-if [[ "${RELEASE_DRY_RUN:-false}" != "true" && "${FORCE_RELEASE:-false}" != "true" && "${BUILDKITE_BRANCH}" != "${RELEASE_VERSION_TAG:-}" ]]; then
+if [[ "${RELEASE_DRY_RUN:-false}" != "true" && "${BUILDKITE_BRANCH}" != "${RELEASE_VERSION_TAG:-}" ]]; then
   echo "Skipping release for a non-tag build on ${BUILDKITE_BRANCH}" >&2
   exit 0
 fi
--- .buildkite/steps/upload-to-gcs.sh
+++ .buildkite/steps/upload-to-gcs.sh
@@ -5,12 +5,27 @@ export GCP_DEFAULT_REGION="us-central1"
 
 VERSION="$(awk -F\" '/const Version/ {print $2}' version/version.go)"
 BASE_BUCKET="buildkite-cloud-functions"
-BUCKET_PATH="buildkite-agent-metrics/builds/${BUILDKITE_BUILD_NUMBER}"
+BUCKET_PATH="buildkite-agent-metrics"
 ARTIFACT="buildkite-agent-metrics.zip"
 
-echo "~~~ :hammer: Creating dummy artifact for testing"
+if [[ "${1:-}" == "release" ]] ; then
+  BUCKET_PATH="${BUCKET_PATH}/v${VERSION}"
+else
+  BUCKET_PATH="${BUCKET_PATH}/builds/${BUILDKITE_BUILD_NUMBER}"
+fi
+
+echo "~~~ :buildkite: Downloading artifacts"
+
 mkdir -p dist
-echo "test" > dist/"${ARTIFACT}"
+if [[ "${1:-}" == "release" ]] ; then
+  buildkite-agent artifact download --build "${BUILDKITE_TRIGGERED_FROM_BUILD_ID}" dist/"${ARTIFACT}" ./dist
+else
+  buildkite-agent artifact download dist/"${ARTIFACT}" ./dist
+fi
 
 echo "--- :GCS: Uploading cloud function to ${BASE_BUCKET}/${BUCKET_PATH}/ in ${GCP_DEFAULT_REGION}"
 gcloud storage cp dist/"${ARTIFACT}" "gs://${BASE_BUCKET}/${BUCKET_PATH}/${ARTIFACT}"
+
+if [[ "${1:-}" == "release" ]] ; then
+  gcloud storage cp "gs://${BASE_BUCKET}/${BUCKET_PATH}/${ARTIFACT}" "gs://${BASE_BUCKET}/buildkite-agent-metrics/cloud-function-latest.zip"
+fi
