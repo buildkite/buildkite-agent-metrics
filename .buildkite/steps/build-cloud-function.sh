8cd3ed24cebddaa8b8b2d3c7b680dc0ebedc238e
Kate Sy <kate.sy@buildkite.com>
2026-05-20 14:37:57 +1000

    fix: zip cloud function

--- .buildkite/steps/build-cloud-function.sh
+++ .buildkite/steps/build-cloud-function.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -eu
 
 # Create a zip file containing just the code required for the cloud function
 
 mkdir -p dist
 rm -f dist/buildkite-agent-metrics.zip
-tar -a -cf dist/buildkite-agent-metrics.zip cloud_function/{main.go,go.mod,go.sum}
+( cd cloud_function && zip ../dist/buildkite-agent-metrics.zip main.go go.mod go.sum )
